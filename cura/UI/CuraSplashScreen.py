c4593d5e62812a95ce98eb9928bd56493498d5ab
Jakub Kozak <43683610+Meph384@users.noreply.github.com>
2022-07-01 01:16:08 +0200

    fix: Restore previous splashscreen settings

--- cura/UI/CuraSplashScreen.py
+++ cura/UI/CuraSplashScreen.py
@@ -14,7 +14,7 @@ import time
 class CuraSplashScreen(QSplashScreen):
     def __init__(self):
         super().__init__()
-        self._scale = 1
+        self._scale = 0.7
         self._version_y_offset = 0  # when extra visual elements are in the background image, move version text down
 
         if ApplicationMetadata.IsAlternateVersion:
@@ -24,26 +24,21 @@ class CuraSplashScreen(QSplashScreen):
             self._version_y_offset = 26
         else:
             splash_image = QPixmap(Resources.getPath(Resources.Images, "cura.png"))
-
         self.setPixmap(splash_image)
-
         self._current_message = ""
-
         self._loading_image_rotation_angle = 0
-
         self._to_stop = False
         self._change_timer = QTimer()
         self._change_timer.setInterval(50)
         self._change_timer.setSingleShot(False)
         self._change_timer.timeout.connect(self.updateLoadingImage)
-
         self._last_update_time = None
 
     def show(self):
         super().show()
         self._last_update_time = time.time()
         self._change_timer.start()
-
+        
     def updateLoadingImage(self):
         if self._to_stop:
             return
@@ -59,31 +54,32 @@ class CuraSplashScreen(QSplashScreen):
 
     def drawContents(self, painter):
         if self._to_stop:
-            return
 
+            return
         painter.save()
         painter.setPen(QColor(255, 255, 255, 255))
-        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
-        painter.setRenderHint(QPainter.RenderHint.Antialiasing, True)
-
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setRenderHint(QPainter.Antialiasing, True)
         version = Application.getInstance().getVersion().split("-")
 
         # Draw version text
-        font = QFont()
-        font.setPixelSize(24)
+        font = QFont()  # Using system-default font here
+        font.setPixelSize(18)
         painter.setFont(font)
-
-        if len(version) == 1:
-            painter.drawText(40, 104 + self._version_y_offset, round(330 * self._scale), round(230 * self._scale), Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop, version[0] if not ApplicationMetadata.IsAlternateVersion else ApplicationMetadata.CuraBuildType)
-        elif len(version) > 1:
-            painter.drawText(40, 104 + self._version_y_offset, round(330 * self._scale), round(230 * self._scale), Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop, f"{version[0]}-{version[1]}" if not ApplicationMetadata.IsAlternateVersion else ApplicationMetadata.CuraBuildType)
+        painter.drawText(60, 70 + self._version_y_offset, round(330 * self._scale), round(230 * self._scale), Qt.AlignLeft | Qt.AlignTop, version[0] if not ApplicationMetadata.IsAlternateVersion else ApplicationMetadata.CuraBuildType)
+        if len(version) > 1:
+            font.setPixelSize(16)
+            painter.setFont(font)
+            painter.setPen(QColor(200, 200, 200, 255))
+            painter.drawText(247, 105 + self._version_y_offset, round(330 * self._scale), round(255 * self._scale), Qt.AlignLeft | Qt.AlignTop, version[1])
+        painter.setPen(QColor(255, 255, 255, 255))
 
         # Draw the loading image
         pen = QPen()
-        pen.setWidthF(2 * self._scale)
-        pen.setColor(QColor(255, 255, 255, 255))
+        pen.setWidthF(6 * self._scale)
+        pen.setColor(QColor(32, 166, 219, 255))
         painter.setPen(pen)
-        painter.drawArc(38, 324, round(20 * self._scale), round(20 * self._scale), round(self._loading_image_rotation_angle * 16), 300 * 16)
+        painter.drawArc(60, 150, round(32 * self._scale), round(32 * self._scale), round(self._loading_image_rotation_angle * 16), 300 * 16)
 
         # Draw message text
         if self._current_message:
@@ -93,24 +89,6 @@ class CuraSplashScreen(QSplashScreen):
             pen.setColor(QColor(255, 255, 255, 255))
             painter.setPen(pen)
             painter.setFont(font)
-            painter.drawText(70, 308, 170, 48,
-                             Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter | Qt.TextFlag.TextWordWrap,
+            painter.drawText(100, 128, 170, 64,
+                             Qt.AlignLeft | Qt.AlignVCenter | Qt.TextWordWrap,
                              self._current_message)
-
-        painter.restore()
-        super().drawContents(painter)
-
-    def showMessage(self, message, *args, **kwargs):
-        if self._to_stop:
-            return
-
-        self._current_message = message
-        self.messageChanged.emit(message)
-        QCoreApplication.processEvents()  # Used to be .flush() -- this might be the closest alternative, but uncertain.
-        self.repaint()
-
-    def close(self):
-        # set stop flags
-        self._to_stop = True
-        self._change_timer.stop()
-        super().close()
